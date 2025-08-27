de78e9fa56c9af3bb4b695d2ad25dc7e58ed86d2
ali aslani <aliaslani.mm@gmail.com>
2025-08-27 22:08:53 +0330

    fix: web metro

---
+++ metro.config.js
@@ -0,0 +1,25 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+const ALIASES = {
+  'tslib': require.resolve('tslib/tslib.es6.js'),
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  // Ensure you call the default resolver.
+ 
+  if (platform === 'web') { 
+        return context.resolveRequest(
+                        context,
+                        // Use an alias if one exists.
+                        ALIASES[moduleName] ?? moduleName,
+                        platform
+                        );
+  }
+  // Ensure you call the default resolver.
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
