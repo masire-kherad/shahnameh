0997de5b026514f6fc0284b0c38a24d857e384c7
ali aslani <aliaslani.mm@gmail.com>
2026-02-03 16:50:45 +0330

    chore: bump version to 1.1.0 in app.config.js and package.json

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => ({
   ...config,
   name: "MasireKherad",
   slug: "shahname",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/images/Logo.png",
   scheme: "shahnameh",
