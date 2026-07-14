c3ed0052481bb40f30d6da946d926134075fc5ca
Kaveh <kavehkazemiseresht@gmail.com>
2026-07-14 20:43:40 +0330

    setting a liitle delay so the sentry could catch error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,7 +39,9 @@ Sentry.init({
   dsn: "https://91f11c7ba06300219ec978417905af45@o4511557557288960.ingest.de.sentry.io/4511731385827408",
   debug: true,
 });
-Sentry.captureMessage("App startup reached _layout");
+setTimeout(() => {
+  Sentry.captureMessage("App startup reached _layout");
+}, 1000);
 
 try {
   I18nManager.allowRTL(true);
